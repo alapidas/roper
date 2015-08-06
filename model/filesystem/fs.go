7a702efbb67f965bec655ee2463bae83d16c8091
Andrew Lapidas <alapidas@gmail.com>
2015-08-05 21:04:47 -0500

    Add filesystem implementation

--- model/filesystem/fs.go
+++ model/filesystem/fs.go
@@ -6,14 +6,22 @@ to a filesystem.
 package filesystem
 
 import (
+	"errors"
 	"fmt"
 	trie "github.com/tchap/go-patricia/patricia"
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"sync"
 )
 
-// A FSFiler knows everything that a normal os.FileInfo does, but it also
+var (
+	ErrFileNoExist = errors.New("file does not exist")
+	ErrPathNoExist = errors.New("path does not exist")
+	ErrFileExists  = errors.New("file already exists")
+)
+
+// FSFiler knows everything that a normal os.FileInfo does, but it also
 // has access to the underlying file.
 type FSFiler interface {
 	os.FileInfo
@@ -33,27 +41,268 @@ func (fsfile *FSFile) File() []byte {
 	return fsfile.file
 }
 
+// Filesystemer is the base interface that any filesystem should implement.
+type Filesystemer interface {
+	//ArbitraryAction()
+	PathExists(path string) bool
+	MkPath(path string) error
+	RmPath(path string) error // Applies to both paths + files
+	MkFile(path string, content []byte) error
+	GetFile(path string) (FSFiler, error)
+	RootPath() string
+}
+
 // A TransientFilesystemer represents an in-memory representation of a filesystem.
 type TransientFilesystemer interface {
 	Filesystemer
 	Sync() error
 }
 
-// Filesystemer is the base interface that any filesystem should implement.
-type Filesystemer interface {
-	PathExists(path string) bool
-	MkPath(path string) error
-	RmPath(path string) error // Applies to both paths + files
-	MkFile(path string, content []byte) error
-	GetFile(path string) (FSFiler, error)
-}
-
+// A transient filesystem has an in-memory representation of an underlying
+// filesystem, for fast access.  The in-memory structure will be updated
+// when paths are added via the object.  When the filesystem is changed
+// outside of this API, Sync() should be called to bring the in-memory
+// representation and actual disk contents back into sync.
 type TransientFilesystem struct {
-	*trie.Trie
-	RootPath string
+	trie     *trie.Trie // not thread safe, and not embedded
+	rootPath string
+	lock     sync.RWMutex
 }
 
-//var _ TransientFilesystemer = (*TransientFilesystem)(nil)
+var _ TransientFilesystemer = (*TransientFilesystem)(nil)
+
+// Sync brings the in-memory struct and underlying file system into sync.
+// Walk the entire tree and recreate it from scratch.
+func (fs *TransientFilesystem) Sync() error {
+	newTrie := trie.NewTrie()
+	err := filepath.Walk(fs.RootPath(), func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		fi, err := os.Stat(path)
+		if err != nil {
+			return err
+		}
+		if !newTrie.Insert(trie.Prefix(path), fi) {
+			return fmt.Errorf("Path %s already exists in tree", path)
+		}
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+
+	fs.lock.Lock()
+	defer fs.lock.Unlock()
+	fs.trie = newTrie
+	return nil
+}
+
+// NewTransientFilesystem returns a new TransientFilesystem.  Expects an
+// absolute path.
+func NewTransientFilesystem(path string) (*TransientFilesystem, error) {
+	fi, err := os.Stat(path)
+	if err != nil {
+		return nil, fmt.Errorf("Unable to stat path %s", path)
+	}
+	if !fi.IsDir() {
+		return nil, fmt.Errorf("Specified path %s is not a directory", path)
+	}
+	fs := &TransientFilesystem{trie: trie.NewTrie(), rootPath: path}
+	if err = fs.Sync(); err != nil {
+		return nil, err
+	}
+	return fs, nil
+}
+
+// RootPath returns the real absolute path of the filesystem
+func (fs *TransientFilesystem) RootPath() string {
+	return fs.rootPath
+}
+
+// PathExists checks to see that the given path exists.
+// The path argument is expected to be relative to the rootPath of the
+// TransientFilesystem.  Works for files and directories.
+func (fs *TransientFilesystem) PathExists(path string) bool {
+	absPath := realPath(fs, path)
+	fs.lock.RLock()
+	defer fs.lock.RUnlock()
+	return fs.trie.Match(trie.Prefix(absPath))
+}
+
+// MkPath makes a new directory under the current rootPath.  The path variable
+// is expected to be relative to the rootPath.  The diectory will both be added
+// to the internal representation AND be added to the real filesystem.  Fails
+// if the  specified path is a file, returns nil if the specified path already
+// exists.
+func (fs *TransientFilesystem) MkPath(path string) error {
+	absPath := realPath(fs, path)
+
+	// actually make path
+	if absPath == fs.RootPath() {
+		return nil
+	}
+	fi, err := os.Stat(absPath)
+	if err != nil && !os.IsNotExist(err) {
+		return ErrFileNoExist
+	}
+	if fi != nil && !fi.IsDir() {
+		return ErrFileExists
+	}
+	doTrieGet := func() (bool, error) {
+		fs.lock.RLock()
+		defer fs.lock.RUnlock()
+		if fs.trie.Match(trie.Prefix(absPath)) {
+			if fs.trie.Get(trie.Prefix(absPath)).(os.FileInfo).IsDir() {
+				return true, nil
+			} else {
+				return false, fmt.Errorf("Specified path %s exists in memory as a file, but does not exist on disk", path)
+			}
+		}
+		return false, nil
+	}
+	existsInMem, err := doTrieGet()
+	if err != nil {
+		return err
+	} else if existsInMem {
+		return nil
+	}
+	if err = os.MkdirAll(absPath, 0755); err != nil {
+		return err
+	}
+	fi, err = os.Stat(absPath)
+	if err != nil {
+		return err
+	}
+
+	// insert into trie
+	insertIntoTrie := func() error {
+		fs.lock.Lock()
+		defer fs.lock.Unlock()
+		if success := fs.trie.Insert(trie.Prefix(absPath), fi); !success {
+			return fmt.Errorf("path %s already exists in memory")
+		}
+		return nil
+	}
+	if err := insertIntoTrie(); err != nil {
+		// recover
+		if rmErr := os.RemoveAll(absPath); rmErr != nil {
+			// wow this is bad, failure inserting into the trie, AND failure
+			// cleaning up after ourselves
+			return fmt.Errorf("failure creating path %s in memory: %s, and failure cleaning up alrready created path %s: %s", path, err.Error(), path, rmErr.Error())
+		}
+		return err
+	}
+	return nil
+}
+
+// RmPath recursively removes a path from the filesystem.  The path argument
+// can be either a file or directory relative to the rootPath.  Returns an
+// error if the path does not exist, or if there was an error removing
+// part of the path.
+func (fs *TransientFilesystem) RmPath(path string) error {
+
+	absPath := realPath(fs, path)
+	switch path {
+	case "/", "":
+		return fmt.Errorf("unwilling to delete root filesystem path %s", fs.RootPath())
+	default:
+	}
+
+	doGet := func() interface{} {
+		fs.lock.RLock()
+		defer fs.lock.RUnlock()
+		return fs.trie.Get(trie.Prefix(absPath))
+	}
+	item := doGet()
+	if item == nil {
+		return ErrPathNoExist
+	}
+	if !item.(os.FileInfo).IsDir() {
+		return ErrFileExists
+	}
+	// Make this a function so we can release the mutex ASAP
+	doTrieDelete := func() error {
+		fs.lock.Lock()
+		defer fs.lock.Unlock()
+		if deleted := fs.trie.Delete(trie.Prefix(absPath)); !deleted {
+			return fmt.Errorf("path %s does not exist in memory", path)
+		}
+		return nil
+	}
+	if err := doTrieDelete(); err != nil {
+		return err
+	}
+	if err := os.RemoveAll(absPath); err != nil {
+		// put item back in the tree
+		// eat the error here
+		fs.trie.Insert(trie.Prefix(absPath), item)
+		// possibly in an unknown state at this point.  whomp whomp.
+		return err
+	}
+	return nil
+}
+
+// MkFile makes a file consisting of the provided content at the given path.
+// If the path does not already exist, an error will be returned.  If the file
+// already exists, an error will be returned.
+func (fs *TransientFilesystem) MkFile(path string, content []byte) error {
+	if !fs.PathExists(filepath.Dir(path)) {
+		return ErrPathNoExist
+	} else if fs.PathExists(path) {
+		return ErrFileExists
+	}
+
+	absPath := realPath(fs, path)
+	if err := ioutil.WriteFile(absPath, content, 0644); err != nil {
+		return err
+	}
+	doInsertTrie := func() error {
+		fs.lock.Lock()
+		defer fs.lock.Unlock()
+		fi, err := os.Stat(absPath)
+		if err != nil {
+			return err
+		}
+		if success := fs.trie.Insert(trie.Prefix(absPath), fi); !success {
+			// path already exists in trie
+			if err := os.RemoveAll(absPath); err != nil {
+				// this is bad
+				return fmt.Errorf("file %s already exists in memory and error removing file %s: %s", path, path, err)
+			}
+			// technically this is a little misleading, as it refers to the
+			// trie, not what's on disk, but c'est la vie
+			return ErrFileExists
+		}
+		return nil
+	}
+	if err := doInsertTrie(); err != nil {
+		return err
+	}
+	return nil
+}
+
+// GetFile gets the file at path and returns a FSFile object, which both
+// describes the file and has a pointer to a copy of the file in a byte slice.
+// If the files does not exist or path is actually a directory, an error is returned.
+func (fs *TransientFilesystem) GetFile(path string) (FSFiler, error) {
+	absPath := realPath(fs, path)
+	if !fs.PathExists(path) {
+		return nil, ErrFileNoExist
+	} else if fi, err := os.Stat(realPath(fs, path)); err != nil || fi.IsDir() {
+		if err != nil {
+			return nil, err
+		}
+		return nil, fmt.Errorf("Specified path %s is a directory", realPath(fs, path))
+	}
+	item := fs.trie.Get(trie.Prefix(absPath))
+	if item == nil {
+		return nil, ErrPathNoExist
+	}
+	fileBytes, _ := ioutil.ReadFile(absPath)
+	fi, _ := os.Stat(realPath(fs, path))
+	return &FSFile{FileInfo: fi, file: fileBytes}, nil
+}
 
 // A passthrough file system doesn't have an internal representation of the
 // actual filesystem - it is merely a proxy to the actual filesystem.  The
@@ -64,7 +313,7 @@ type TransientFilesystem struct {
 // If you want to be able to set custom ownership or permissions, provide your own
 // implementation.
 type PassThroughFilesystem struct {
-	RootPath string
+	rootPath string
 }
 
 var _ Filesystemer = (*PassThroughFilesystem)(nil)
@@ -80,49 +329,68 @@ func NewPassThroughFilesystem(path string) (*PassThroughFilesystem, error) {
 	if !fi.IsDir() {
 		return nil, fmt.Errorf("Specified path %s is not a directory", path)
 	}
-	return &PassThroughFilesystem{RootPath: path}, nil
+	return &PassThroughFilesystem{rootPath: path}, nil
+}
+
+// RootPath returns the real absolute path of the filesystem
+func (fs *PassThroughFilesystem) RootPath() string {
+	return fs.rootPath
 }
 
 // realPath returns the absolute path of the PassThroughFilesystem's
-// RootPath plus the given relPath.
-func (fs *PassThroughFilesystem) realPath(relPath string) string {
-	tgtPath := filepath.Join(fs.RootPath, relPath)
+// rootPath plus the given relPath.
+func realPath(fs Filesystemer, relPath string) string {
+	tgtPath := filepath.Join(fs.RootPath(), relPath)
 	return filepath.Clean(tgtPath)
 }
 
-// PathExists checks to see that the given path exist.
-// The path argument is expected to be relative to the RootPath of the
+// PathExists checks to see that the given path exists.
+// The path argument is expected to be relative to the rootPath of the
 // PassThroughFilesystem.  Works for files and directories.
 func (fs *PassThroughFilesystem) PathExists(path string) bool {
-	_, err := os.Stat(fs.realPath(path))
+	_, err := os.Stat(realPath(fs, path))
 	return !os.IsNotExist(err)
 }
 
-// MkPath makes a new directory under the current RootPath.  The path variable
-// is expected to be relative to the RootPath
+// MkPath makes a new directory under the current rootPath.  The path variable
+// is expected to be relative to the rootPath.  Returns nil if the path already
+// exists as a dir, and an error if the path already exists and is a file.
 func (fs *PassThroughFilesystem) MkPath(path string) error {
-	absPath := fs.realPath(path)
-	if absPath == fs.RootPath {
+	absPath := realPath(fs, path)
+	if absPath == fs.rootPath {
 		return nil
+	}
+	fi, err := os.Stat(absPath)
+	if err != nil && !os.IsNotExist(err) {
+		return err
+	}
+	if fi != nil && !fi.IsDir() {
+		return ErrFileExists
 	}
 	return os.MkdirAll(absPath, 0755)
 }
 
-// RmPath recursively removes a path from the filesystem.  The path argument
-// can be either a file or directory relative to the RootPath.  Returns an
-// error if the path does not exist, or if there was an error removing
-// part of the path.
-func (fs *PassThroughFilesystem) RmPath(path string) error {
-	absPath := fs.realPath(path)
+// rmPathOnDisk removes the specified path from disk.  The given path is
+// relative to the root of the associated Filesystemer object.
+func rmPathOnDisk(fs Filesystemer, path string) error {
+	absPath := realPath(fs, path)
 	switch path {
 	case "/", "":
-		return fmt.Errorf("Unwilling to delete root filesystem path %s", fs.RootPath)
+		return fmt.Errorf("unwilling to delete root filesystem path %s", fs.RootPath())
 	default:
 	}
 	if !fs.PathExists(path) {
-		return fmt.Errorf("Specified path %s does not exist", absPath)
+		return ErrPathNoExist
 	}
 	return os.RemoveAll(absPath)
+}
+
+// RmPath recursively removes a path from the filesystem.  The path argument
+// can be either a file or directory relative to the rootPath.  Returns an
+// error if the path does not exist, or if there was an error removing
+// part of the path.
+func (fs *PassThroughFilesystem) RmPath(path string) error {
+	return rmPathOnDisk(fs, path)
 }
 
 // MkFile makes a file consisting of the provided content at the given path.
@@ -130,12 +398,12 @@ func (fs *PassThroughFilesystem) RmPath(path string) error {
 // already exists, an error will be returned.
 func (fs *PassThroughFilesystem) MkFile(path string, content []byte) error {
 	if !fs.PathExists(filepath.Dir(path)) {
-		return fmt.Errorf("Parent path %s does not exist", fs.realPath(filepath.Dir(path)))
+		return ErrPathNoExist
 	} else if fs.PathExists(path) {
-		return fmt.Errorf("File %s already exists", fs.realPath(path))
+		return ErrFileExists
 	}
 
-	return ioutil.WriteFile(fs.realPath(path), content, 0666)
+	return ioutil.WriteFile(realPath(fs, path), content, 0644)
 }
 
 // GetFile gets the file at path and returns a FSFile object, which both
@@ -143,14 +411,14 @@ func (fs *PassThroughFilesystem) MkFile(path string, content []byte) error {
 // If the files does not exist or path is actually a directory, an error is returned.
 func (fs *PassThroughFilesystem) GetFile(path string) (FSFiler, error) {
 	if !fs.PathExists(path) {
-		return nil, fmt.Errorf("File %s does not exist", fs.realPath(path))
-	} else if fi, err := os.Stat(fs.realPath(path)); err != nil || fi.IsDir() {
+		return nil, ErrFileNoExist
+	} else if fi, err := os.Stat(realPath(fs, path)); err != nil || fi.IsDir() {
 		if err != nil {
 			return nil, err
 		}
-		return nil, fmt.Errorf("Specified path %s is a directory", fs.realPath(path))
+		return nil, fmt.Errorf("Specified path %s is a directory", realPath(fs, path))
 	}
-	fileBytes, _ := ioutil.ReadFile(fs.realPath(path))
-	fi, _ := os.Stat(fs.realPath(path))
+	fileBytes, _ := ioutil.ReadFile(realPath(fs, path))
+	fi, _ := os.Stat(realPath(fs, path))
 	return &FSFile{FileInfo: fi, file: fileBytes}, nil
 }
--- model/filesystem/fs_test.go
+++ model/filesystem/fs_test.go
@@ -1,6 +1,7 @@
 package filesystem
 
 import (
+	trie "github.com/tchap/go-patricia/patricia"
 	. "gopkg.in/check.v1"
 	"io/ioutil"
 	"os"
@@ -22,9 +23,45 @@ func (suite *TheSuite) SetUpTest(c *C) {
 }
 
 // helpers
-func (suite *TheSuite) mkFS() *PassThroughFilesystem {
+func (suite *TheSuite) mkPTFS() *PassThroughFilesystem {
 	fs, _ := NewPassThroughFilesystem(suite.tmpdir)
 	return fs
+}
+
+func (suite *TheSuite) mkTFS() *TransientFilesystem {
+	fs, _ := NewTransientFilesystem(suite.tmpdir)
+	return fs
+}
+
+func (suite *TheSuite) mkDirs(c *C, dir string) map[string]string {
+	fileMap := make(map[string]string)
+	f1, err := ioutil.TempFile(suite.tmpdir, "f1")
+	if err != nil {
+		c.Error(err)
+	}
+	fileMap["f1"] = f1.Name()
+	d1 := filepath.Join(suite.tmpdir, "d1")
+	fileMap["d1"] = d1
+	os.MkdirAll(d1, 0755)
+	f2, err := ioutil.TempFile(d1, "f2")
+	if err != nil {
+		c.Error(err)
+	}
+	fileMap["f2"] = f2.Name()
+	f3, err := ioutil.TempFile(d1, "f3")
+	if err != nil {
+		c.Error(err)
+	}
+	fileMap["f3"] = f3.Name()
+	return fileMap
+}
+
+// Log the whole trie
+func (suite *TheSuite) TrieLog(c *C, fs *TransientFilesystem) {
+	fs.trie.VisitSubtree(trie.Prefix(""), func(prefix trie.Prefix, item trie.Item) error {
+		c.Logf("%q: %v\n", prefix, item)
+		return nil
+	})
 }
 
 // tests
@@ -43,22 +80,22 @@ func (suite *TheSuite) TestPassThroughFilesystemerFileNotDir(c *C) {
 }
 
 func (suite *TheSuite) TestPassThroughFilesystemerCreate(c *C) {
-	fs := suite.mkFS()
-	c.Assert(fs.RootPath, Equals, suite.tmpdir)
+	fs := suite.mkPTFS()
+	c.Assert(fs.rootPath, Equals, suite.tmpdir)
 }
 
 func (suite *TheSuite) TestPassThroughFilesystemer_realPath(c *C) {
-	fs := suite.mkFS()
-	f, _ := ioutil.TempFile(fs.RootPath, "apples")
+	fs := suite.mkPTFS()
+	f, _ := ioutil.TempFile(fs.rootPath, "apples")
 	f.Close()
 	tmpfileName := filepath.Base(f.Name())
-	absPath := fs.realPath(tmpfileName)
-	c.Assert(absPath, Equals, filepath.Clean(filepath.Join(fs.RootPath, tmpfileName)))
+	absPath := realPath(fs, tmpfileName)
+	c.Assert(absPath, Equals, filepath.Clean(filepath.Join(fs.rootPath, tmpfileName)))
 }
 
 func (suite *TheSuite) TestPassThroughFilesystemer_PathExists(c *C) {
-	fs := suite.mkFS()
-	f, _ := ioutil.TempFile(fs.RootPath, "apples")
+	fs := suite.mkPTFS()
+	f, _ := ioutil.TempFile(fs.rootPath, "apples")
 	f.Close()
 	tmpfileName := filepath.Base(f.Name())
 	c.Assert(fs.PathExists(tmpfileName), Equals, true)
@@ -66,26 +103,30 @@ func (suite *TheSuite) TestPassThroughFilesystemer_PathExists(c *C) {
 }
 
 func (suite *TheSuite) TestPassThroughFilesystemer_MkPath(c *C) {
-	fs := suite.mkFS()
+	fs := suite.mkPTFS()
 	err := fs.MkPath("ab/cd")
 	c.Assert(err, IsNil)
 	c.Assert(fs.PathExists("ab/cd"), Equals, true)
-	fs = suite.mkFS()
+	fs = suite.mkPTFS()
 	err = fs.MkPath("/ab/cd")
 	c.Assert(fs.PathExists("/ab/cd"), Equals, true)
 	c.Assert(err, IsNil)
 	err = fs.MkPath("")
 	c.Assert(err, IsNil)
+	err = fs.MkFile("here", []byte{})
+	c.Assert(err, IsNil)
+	err = fs.MkPath("here")
+	c.Assert(err, Equals, ErrFileExists)
 }
 
 func (suite *TheSuite) TestPassThroughFilesystemer_RmPath(c *C) {
-	fs := suite.mkFS()
+	fs := suite.mkPTFS()
 	fs.MkPath("abc/123")
 	c.Assert(fs.PathExists("abc/123"), Equals, true)
 	err := fs.RmPath("abc/123")
 	c.Assert(err, IsNil)
 	err = fs.RmPath("abc/notexist")
-	c.Assert(err, NotNil)
+	c.Assert(err, Equals, ErrPathNoExist)
 	err = fs.RmPath("")
 	c.Assert(err, NotNil)
 	err = fs.RmPath("/")
@@ -94,9 +135,9 @@ func (suite *TheSuite) TestPassThroughFilesystemer_RmPath(c *C) {
 }
 
 func (suite *TheSuite) TestPassThroughFilesystemer_MkFile(c *C) {
-	fs := suite.mkFS()
+	fs := suite.mkPTFS()
 	err := fs.MkFile("this/path/here", []byte{})
-	c.Assert(err, NotNil) //parent path does not exist
+	c.Assert(err, Equals, ErrPathNoExist) //parent path does not exist
 	err = fs.MkFile("here", []byte{})
 	c.Assert(err, IsNil)
 
@@ -105,18 +146,117 @@ func (suite *TheSuite) TestPassThroughFilesystemer_MkFile(c *C) {
 	fs.MkPath("myDir")
 	err = fs.MkFile("myDir/theFile", fileBytes)
 	c.Assert(err, IsNil)
-	absPath := fs.realPath("myDir/theFile")
+	absPath := realPath(fs, "myDir/theFile")
 	newBytes, _ := ioutil.ReadFile(absPath)
 	c.Assert(fileBytes, DeepEquals, newBytes)
 
 	err = fs.MkFile("myDir/theFile", fileBytes)
-	c.Assert(err, NotNil) //file already exists
+	c.Assert(err, Equals, ErrFileExists) //file already exists
 }
 
 func (suite *TheSuite) TestPassThroughFilesystemer_GetFile(c *C) {
-	fs := suite.mkFS()
+	fs := suite.mkPTFS()
 	_, err := fs.GetFile("dat/path") //file doesn't exist
+	c.Assert(err, Equals, ErrFileNoExist)
+	err = fs.MkPath("ab/cd")
+	_, err = fs.GetFile("ab/cd") //actually a dir
 	c.Assert(err, NotNil)
+
+	cwd, _ := os.Getwd()
+	fileBytes, _ := ioutil.ReadFile(filepath.Join(cwd, "thetestfile.txt"))
+	fs.MkPath("myDir")
+	fs.MkFile("myDir/theFile", fileBytes)
+	c.Log("MADE IT HERE")
+
+	newFile, err := fs.GetFile("myDir/theFile")
+	c.Assert(err, IsNil)
+	c.Assert(fileBytes, DeepEquals, newFile.File())
+
+}
+
+func (suite *TheSuite) TestTransientFilesystem_Sync(c *C) {
+	fs := suite.mkTFS()
+	fileMap := suite.mkDirs(c, suite.tmpdir)
+	if err := fs.Sync(); err != nil {
+		c.Error(err)
+	}
+	c.Assert(fs.trie.Get(trie.Prefix("aaaaaaaaaaa")), IsNil)
+	c.Assert(fs.trie.Get(trie.Prefix(fs.rootPath)), NotNil)
+	c.Assert(fs.trie.Get(trie.Prefix(fileMap["d1"])), NotNil)
+	c.Assert(fs.trie.Get(trie.Prefix(fileMap["f1"])), NotNil)
+	c.Assert(fs.trie.Get(trie.Prefix(fileMap["f2"])), NotNil)
+	c.Assert(fs.trie.Get(trie.Prefix(fileMap["f3"])), NotNil)
+}
+
+func (suite *TheSuite) TestTransientFilesystemerCreate(c *C) {
+	fileMap := suite.mkDirs(c, suite.tmpdir)
+	fs := suite.mkTFS()
+	c.Assert(fs.trie.Get(trie.Prefix("aaaaaaaaaaa")), IsNil)
+	c.Assert(fs.trie.Get(trie.Prefix(fs.rootPath)), NotNil)
+	c.Assert(fs.trie.Get(trie.Prefix(fileMap["d1"])), NotNil)
+	c.Assert(fs.trie.Get(trie.Prefix(fileMap["f1"])), NotNil)
+	c.Assert(fs.trie.Get(trie.Prefix(fileMap["f2"])), NotNil)
+	c.Assert(fs.trie.Get(trie.Prefix(fileMap["f3"])), NotNil)
+}
+
+func (suite *TheSuite) TestTransientFilesystemer_PathExists(c *C) {
+	suite.mkDirs(c, suite.tmpdir)
+	fs := suite.mkTFS()
+	c.Assert(fs.PathExists("d1"), Equals, true)
+	c.Assert(fs.PathExists("/blah"), Equals, false)
+}
+
+func (suite *TheSuite) TestTransientFilesystemer_MkPath(c *C) {
+	fileMap := suite.mkDirs(c, suite.tmpdir)
+	fs := suite.mkTFS()
+	err := fs.MkPath("")
+	c.Assert(err, IsNil)
+	suite.TrieLog(c, fs)
+	err = fs.MkPath(filepath.Base(fileMap["f1"]))
+	c.Assert(err, Equals, ErrFileExists)
+	err = fs.MkPath("d1")
+	c.Assert(err, IsNil)
+	err = fs.MkPath("d2")
+	c.Assert(err, IsNil)
+}
+
+func (suite *TheSuite) TestTransientFilesystemer_RmPath(c *C) {
+	fileMap := suite.mkDirs(c, suite.tmpdir)
+	fs := suite.mkTFS()
+	err := fs.RmPath(filepath.Base(fileMap["f1"]))
+	c.Assert(err, Equals, ErrFileExists)
+	err = fs.RmPath("d1")
+	c.Assert(err, IsNil)
+	_, err = os.Stat(fileMap["d1"])
+	c.Assert(os.IsNotExist(err), Equals, true)
+}
+
+func (suite *TheSuite) TestTransientFilesystemer_MkFile(c *C) {
+	fs := suite.mkTFS()
+	err := fs.MkFile("this/path/here", []byte{})
+	c.Assert(err, Equals, ErrPathNoExist) //parent path does not exist
+	err = fs.MkFile("here", []byte{})
+	c.Assert(err, IsNil)
+
+	cwd, _ := os.Getwd()
+	fileBytes, _ := ioutil.ReadFile(filepath.Join(cwd, "thetestfile.txt"))
+	err = fs.MkPath("myDir")
+	suite.TrieLog(c, fs)
+	c.Assert(err, IsNil)
+	err = fs.MkFile("myDir/theFile", fileBytes)
+	c.Assert(err, IsNil)
+	absPath := realPath(fs, "myDir/theFile")
+	newBytes, _ := ioutil.ReadFile(absPath)
+	c.Assert(fileBytes, DeepEquals, newBytes)
+
+	err = fs.MkFile("myDir/theFile", fileBytes)
+	c.Assert(err, Equals, ErrFileExists) //file already exists
+}
+
+func (suite *TheSuite) TestTransientFilesystemer_GetFile(c *C) {
+	fs := suite.mkTFS()
+	_, err := fs.GetFile("dat/path") //file doesn't exist
+	c.Assert(err, Equals, ErrFileNoExist)
 	err = fs.MkPath("ab/cd")
 	_, err = fs.GetFile("ab/cd") //actually a dir
 	c.Assert(err, NotNil)
