688a550bb232cac1a10ab61ab471bea6537f0f87
Andrew Lapidas <alapidas@gmail.com>
2016-01-21 15:18:23 -0600

    Make main handle shutdown signals OK

--- interfaces/web.go
+++ interfaces/web.go
@@ -1,6 +1,8 @@
 package interfaces
 
 import (
+	log "github.com/Sirupsen/logrus"
+	"github.com/gorilla/mux"
 	"net/http"
 )
 
@@ -10,9 +12,32 @@ type DirConfig struct {
 }
 
 // StartWeb simply provides a web server for the files in repos
-func StartWeb(dirs []DirConfig) error {
+func StartWeb(dirs []DirConfig, shutdownChan chan struct{}) error {
+	r := mux.NewRouter()
+	prefixes := make([]string, len(dirs))
 	for _, dir := range dirs {
-		http.Handle("/"+dir.TopLevel, http.StripPrefix("/"+dir.TopLevel, http.FileServer(http.Dir(dir.AbsPath))))
+		prefixes = append(prefixes, dir.TopLevel)
+		r.PathPrefix(
+			"/" + dir.TopLevel + "/",
+		).Handler(
+			http.StripPrefix(
+				"/"+dir.TopLevel+"/", http.FileServer(http.Dir(dir.AbsPath+"/")),
+			),
+		)
 	}
-	return http.ListenAndServe(":3000", nil)
+	http.Handle("/", r)
+	log.WithFields(log.Fields{
+		"prefixes": prefixes,
+	}).Infof("Starting web server for repos at prefixes")
+	webDoneChan := make(chan error)
+	go func() {
+		webDoneChan <- http.ListenAndServe(":3000", nil)
+	}()
+	select {
+	case err := <-webDoneChan:
+		log.Warnf("Web server exited: %s", err)
+	case <-shutdownChan:
+		log.Warn("Web server received shutdown signal")
+	}
+	return nil
 }
